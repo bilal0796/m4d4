88bdb7c7e66e968735e35e4eab1aaf13b26fe81c
Bilal Ahmad <bilal.ahmad0796@gmail.com>
2021-11-11 14:50:02 +0100

    SingleBook, BookList added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import SingleBook from './Components/SingleBook';
+import booksdata from './Data/SciFi.json'
+import BookList from './Components/Booklist';
 
 function App() {
-  return (
-  );
+  return (<>
+   <div className='container'>
+   <SingleBook book={booksdata[4]} />
+   <BookList books={booksdata} />
+   </div>
+  </>)
 }
 
 export default App;
